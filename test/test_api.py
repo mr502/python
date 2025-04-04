49090f440cca17993e17b22e2af09192bf06a226
skmengrui <skmengrui@163.com>
2025-04-04 22:46:56 +0800

    new a test

---
+++ test/test_api.py
@@ -0,0 +1,18 @@
+test_api = {
+    "test_api": {
+        "name": "test_api",
+        "type": "test_api",
+        "description": "test_api",
+        "inputs": [
+            {
+                "name": "input_1",
+                "type": "string",
+                "description": "input_1",
+                "required": True,
+                "default": "input_1",
+                "options": ["input_1", "input_2"],
+            },
+            {}
+        ]
+    }
+}
